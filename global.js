ceb2af3b16f552247b9546d867ff5f3b2477a0df
Pranav Rajaram <prajaram@ucsd.edu>
2025-01-27 14:46:57 -0800

    buddy hield

--- global.js
+++ global.js
@@ -87,11 +87,56 @@ if ("colorScheme" in localStorage) {
 
 select.addEventListener('input', function (event) {
 
-    setColorScheme(event.target.value);
-    localStorage.colorScheme = event.target.value;
-    console.log('color scheme changed to', event.target.value);
+  setColorScheme(event.target.value);
+  localStorage.colorScheme = event.target.value;
+  console.log('color scheme changed to', event.target.value);
 
-  });
+});
 
 
+export async function fetchJSON(url) {
+  try {
+      // Fetch the JSON file from the given URL
+      const response = await fetch(url);
+
+      if (!response.ok) {
+        throw new Error(`Failed to fetch projects: ${response.statusText}`);
+    }
+
+    console.log(response);
+
+    const data = await response.json();
+    return data; 
+
+  } catch (error) {
+      console.error('Error fetching or parsing JSON data:', error);
+  }
+}
+
+export function renderProjects(project, containerElement, headingLevel = 'h2') {
   
+  containerElement.innerHTML = '';
+
+  project.forEach(project => {
+    const article = document.createElement('article');
+
+    article.innerHTML = `
+    <h3>
+    <a href="${project.link}" target="_blank">${project.title}</a>
+    </h3>
+    <img src="${project.image}" alt="${project.title}">
+    <p>${project.description}</p>`;
+    
+    containerElement.appendChild(article);
+});
+
+
+}
+
+
+export async function fetchGitHubData(username) {
+  return fetchJSON(`https://api.github.com/users/${username}`);
+
+}
+
+
---
+++ index.js
@@ -0,0 +1,25 @@
+import { fetchJSON, renderProjects, fetchGitHubData } from './global.js';
+
+const projects = await fetchJSON('../lib/projects.json');
+const latestProjects = projects.slice(0, 3);
+
+const projectsContainer = document.querySelector('.projects');
+
+renderProjects(latestProjects, projectsContainer, 'h2');
+
+
+const githubData = await fetchGitHubData('pranavrajaram');
+
+const profileStats = document.querySelector('#profile-stats');
+
+if (profileStats) {
+    profileStats.innerHTML = `
+          <dl>
+            <dt>Username:</dt><dd>${githubData.login}</dd>
+            <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
+            <dt>Followers:</dt><dd>${githubData.followers}</dd>
+            <dt>Following:</dt><dd>${githubData.following}</dd>
+          </dl>
+      `;
+  }
+
